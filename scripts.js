69c337307c137976e0c1a36a35127b59e81429ba
Rodrigo Tabeira <138072017+rodrigotabeira@users.noreply.github.com>
2024-10-23 11:20:41 -0300

    CHANGES

--- scripts.js
+++ scripts.js
@@ -3,11 +3,8 @@ let images = [
     'imagenes/imagen2.jpg', 
     'imagenes/imagen3.png', 
     'imagenes/imagen4.png', 
-    'imagenes/imagen5.jpg', 
-    'imagenes/imagen6.png', 
-    'imagenes/imagen7.png', 
+    'imagenes/imagen5.png',
     'imagenes/imagen8.jpg', 
-    'imagenes/imagen9.png', 
     'imagenes/imagen10.png', 
     'imagenes/imagen11.png', 
     'imagenes/imagen12.png', 
@@ -16,19 +13,15 @@ let images = [
     'imagenes/imagen15.png', 
     'imagenes/imagen16.png', 
     'imagenes/imagen17.png', 
-    'imagenes/imagen18.png', 
     'imagenes/imagen19.png', 
     'imagenes/imagen20.png', 
     'imagenes/imagen21.png',
-    'imagenes/imagen22.jpg',
     'imagenes/imagen23.jpeg',
     'imagenes/imagen24.jpeg',
     'imagenes/imagen25.jpg',
-    'imagenes/imagen26.jpeg',
     'imagenes/imagen27.png',
     'imagenes/imagen28.png',
-    'imagenes/imagen29.png',
-    'imagenes/imagen30.png'
+    'imagenes/imagen29.png'
 ];
 
 let currentIndex = 0;
