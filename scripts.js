a2bddd6d6a680b033592c40fa7f4ffc56c86f158
Rodrigo Tabeira <138072017+rodrigotabeira@users.noreply.github.com>
2024-12-26 10:08:11 -0300

    Lucas 26-12

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 let images = [
     'imagenes/imagen1.png',
+    'imagenes/imagen32.png',
+    'imagenes/imagen31.png',
     'imagenes/imagen30.png',
     'imagenes/imagen29.png',
     'imagenes/imagen28.png',
