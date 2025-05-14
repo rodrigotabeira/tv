18c3762ca52760a81246a8a86b7001b582ecac98
Rodrigo Tabeira <cofuemamarketing@gmail.com>
2025-05-14 16:20:33 -0300

    Lucas 14-05

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let images = [
     'imagenes/imagen41.png',
     'imagenes/imagen3.png',
+    'imagenes/imagen14.png',
     'imagenes/imagen12.png',
     'imagenes/imagen9.png',
     'imagenes/imagen6.png',
