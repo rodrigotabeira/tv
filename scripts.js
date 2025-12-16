bd869c72e9dc9800251f05fded54dd33db657818
Rodrigo Tabeira <138072017+rodrigotabeira@users.noreply.github.com>
2025-12-16 11:32:14 -0300

    Actualización 16/12 3

--- scripts.js
+++ scripts.js
@@ -24,9 +24,7 @@ let images = [
     'imagenes/imagen23.png',
     'imagenes/imagen24.png',
     'imagenes/imagen25.png',
-    'imagenes/imagen26.png',
-    'imagenes/imagen27.png',
-    'imagenes/imagen28.png',
+    'imagenes/imagen26.png'
 ];
 
 let currentIndex = 0;
