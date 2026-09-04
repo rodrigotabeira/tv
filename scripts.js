75d98f936c9310656e49e6f6d68d6d348efc15e7
Rodrigo Tabeira <cofuemamarketing@gmail.com>
2026-09-04 14:06:41 -0300

    Modificaciones 2 4-9

--- scripts.js
+++ scripts.js
@@ -18,11 +18,7 @@ let images = [
     'imagenes/imagen17.png',
     'imagenes/imagen18.png',
     'imagenes/imagen19.png',
-    'imagenes/imagen20.png',
-    'imagenes/imagen21.png',
-    'imagenes/imagen22.png',
-    
-
+    'imagenes/imagen20.png'
 ];
 
 let currentIndex = 0;
